56daeebd4fc78b379b1ed4b9d28fe99136dd1b20
Veil <65228307+auphong2707@users.noreply.github.com>
2025-12-25 03:49:59 +0700

    Revert "Update utilities.py"
    This reverts commit 05beb4f9704a92481366e3789c74bcbeb112c95e.

--- phase-6-final-system-evaluation/utilities.py
+++ phase-6-final-system-evaluation/utilities.py
@@ -941,8 +941,9 @@ class ROICalculator:
         # How much we save by using the model vs doing nothing
         total_savings = baseline_cost - cost_with_intervention
         
-        # Cost savings = baseline cost - expected value
-        cost_savings = baseline_cost - expected_value
+        # Cost savings = same as total_savings in cost accounting framework
+        # This represents: baseline cost (positive) - cost with model (positive) = savings
+        cost_savings = total_savings
         
         # ROI calculation (return on intervention investment)
         # Net benefit (expected_value) relative to intervention cost
